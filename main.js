1de56858239b110b57031238ff3242fd3b46a74f
Alberto Botero <alberto_botero@hotmail.com>
2019-03-07 20:53:58 -0500

    20190307

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).ready(function (){
     console.log("You are reading this from the console");
+
+    $("h1").css("color", "red");
+    
+    paper.install(window);
+    paper.setup($("#mainCanvas")[0]);
+    
+    var tool = new Tool();
+
+    tool.onMouseDown = function (event) {
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+
+        c.fillColor = "green";
+    }
+
+
+    /*
+    var c = Shape.Circle(200,200, 50);
+
+    c.fillColor = "green";
+    */
+
+
+    paper.view.draw();
 });
